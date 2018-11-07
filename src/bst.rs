a9641ffb0f25a21132fcf1b60db6b34d06a8757f
KellanMebane <kellanmebane@gmail.com>
2018-11-06 22:21:57 -0800

    got some docs going

--- src/bst.rs
+++ src/bst.rs
@@ -1,137 +1,279 @@
-// array based BST (SEQUENTIAL)
+/// Simple Tuple Wrapper for the key value pair.
+/// This lets each index just be a single option
+#[derive(Clone, Copy)]
+pub struct GPair<K, V>(K, V);
 
-#[derive(Clone)]
-pub struct Pair(char, String);
-
-#[allow(dead_code)]
-impl Pair {
-    pub fn new(c: char, s: String) -> Pair {
-        Pair { 0: c, 1: s }
+/// Allocate as a tuple of values
+impl<K, V> GPair<K, V> {
+    pub fn new(key: K, val: V) -> GPair<K, V> {
+        GPair { 0: key, 1: val }
     }
 }
 
-pub struct BST {
-    size: usize,
-    capacity: usize,
-    vec: Vec<Option<Pair>>, // vector of optional indeces containing
-                            // either none or some pair of character and string
+/// Actual BST, accepts key value pairs.
+/// Keeps state of it's size and capacity.
+/// Keeps track of the iterator through
+/// previously visited item stack and
+/// current index when traversing.
+/// Current is an optional, is None when an index can't be found
+/// Tree is implemented in vector array and
+/// grows based on level.
+/// Capacity needed is Σ(2^L), where L is level starting from 0
+#[derive(Clone)]
+pub struct GBST<K, V> {
+    size: usize,            // number of elements currently in the tree
+    capacity: usize,        // size of vector needed, this growns with the tree as it get's deeper
+    current: Option<usize>, //index of current iterator node
+    stack: Vec<usize>,
+    first: bool,
+    vec: Vec<Option<GPair<K, V>>>, // vector of optional indeces containing either none or some pair of character and string
 }
 
-// parent(index) = [(index - 1) / 2] if r != 0
-// left(index) = 2(index) + 1 if 2(index) + 1 <= size
-// right(index) = 2(index) + 2 if 2(index + 2) <= size
+/// Impl methods
 #[allow(dead_code)]
-impl BST {
-    pub fn new() -> BST {
-        BST {
+impl<K, V> GBST<K, V> {
+
+    /// "Constructor" method. Returns an allocated BST
+    /// Requires Clone for key and value types.
+    /// Capacity initialized at 127 which is a depth of 6.
+    /// This is planned to be modular.
+    pub fn new() -> GBST<K, V>
+    where
+        K: Clone,
+        V: Clone,
+    {
+        GBST {
             size: 0,
-            capacity: 100,
-            vec: vec![None; 100],
+            capacity: 127, // enough for depth of 6 initially
+            current: Some(0),
+            stack: Vec::new(),
+            first: true,
+            vec: vec![None; 127],
         }
     }
 
+    /// Returns true if empty
     pub fn is_empty(&self) -> bool {
         self.size == 0
     }
 
-    pub fn insert(&mut self, item: Pair) -> bool {
-        self.internal_insert(0, item)
+    /// Returns current allocated Tree capacity
+    pub fn capacity(&self) -> usize {
+        self.capacity
     }
 
-    // basically checks if the index is used
-    // returns the char at the index or null char
-    fn get_char(&self, index: usize) -> char {
+    /// Returns number of elements in the Tree
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    /// Return an optional reference to the key at index
+    pub fn get_key(&self, index: usize) -> Option<&K> {
         match self.vec[index].as_ref().as_ref() {
-            Some(value) => value.0,
-            None => 0 as char, // null I think lmao
+            Some(pair) => Some(&pair.0),
+            None => None,
         }
     }
 
-    fn get_string(&self, index: usize) -> String {
+    /// Return an optional reference to the value at index
+    fn get_val(&self, index: usize) -> Option<&V> {
         match self.vec[index].as_ref().as_ref() {
-            Some(value) => {
-                let x = (&value.1).to_string();
-                //"cool".to_string()
-                //println!("{}", x);
-                x
-            }
-            None => "".to_string(), // null I think lmao
+            Some(pair) => Some(&pair.1),
+            None => None,
         }
     }
 
-    fn internal_insert(&mut self, index: usize, item: Pair) -> bool {
-        let key = self.get_char(index);
+    /// Insert, calls recursive insertion method
+    /// Requires types to have >, <, == operators to be implemented
+    /// Returns false if the value can't be placed
+    pub fn insert(&mut self, key: K, val: V) -> bool
+    where
+        K: PartialEq + PartialOrd,
+    {
+        self.internal_insert(0, GPair::new(key, val))
+    }
 
-        // this means we have an unused index
-        // insert here
-        if key == 0 as char {
-            self.vec[index] = Some(item);
+    /// Insert recursive function
+    /// Kinda ugly right now
+    fn internal_insert(&mut self, index: usize, pair: GPair<K, V>) -> bool
+    where
+        K: PartialEq + PartialOrd,
+    {
+        if index > self.capacity {
+            return false;
+        }
+
+        let is_empty_leaf: bool;
+        {
+            is_empty_leaf = self.get_key(index).is_none();
+        }
+
+        if is_empty_leaf {
+            self.vec[index] = Some(pair);
             self.size += 1;
             return true;
         }
 
-        if item.0 == key {
-            return false;
-        } else if item.0 > key {
-            let mut new_index = 0;
-            if (2 * index) + 2 <= self.capacity {
-                new_index = (2 * index) + 2;
+        let mut new_index = 0;
+        {
+            let key: &K = self.get_key(index).unwrap();
+
+            if pair.0 == *key {
+                return false;
+            } else if pair.0 > *key {
+                if (2 * index) + 2 <= self.capacity {
+                    new_index = (2 * index) + 2;
+                } else {
+                    return false;
+                }
+            } else {
+                if (2 * index) + 1 <= self.capacity {
+                    new_index = (2 * index) + 1;
+                } else {
+                    return false;
+                }
             }
-            return self.internal_insert(new_index, item);
-        } else {
-            let mut new_index = 0;
-            if (2 * index) + 1 <= self.capacity {
-                new_index = (2 * index) + 1;
-            }
-            return self.internal_insert(new_index, item);
+        }
+        return self.internal_insert(new_index, pair);
+    }
+
+    /// Iterate in order (low -> high)
+    /// matches on next() and optionally returns a reference to the key
+    /// next() gives the index, iter() get's the key
+    pub fn iter(&mut self) -> Option<&K> {
+        match self.next() {
+            Some(x) => self.get_key(x),
+            None => None,
         }
     }
 
-    pub fn inorder(&self) {
-        self.internal_inorder(0);
-    }
+    /// Search by key
+    /// Iterative, searches based on binary comparisons
+    pub fn contains_key(&self, key: &K) -> Option<usize>
+    where
+        K: PartialEq + PartialOrd,
+    {
+        let mut index: usize = 0;
 
-    fn internal_inorder(&self, index: usize) {
-        let key = self.get_char(index);
+        let mut x_key = self.get_key(index);
 
-        if key == 0 as char {
-            return;
+        while x_key.is_some() {
+            let data = x_key.unwrap();
+            if *data > *key {
+                index = index * 2 + 1;
+            } else if *data < *key {
+                index = index * 2 + 2;
+            } else {
+                return Some(index);
+            }
+
+            x_key = self.get_key(index);
         }
 
-        self.internal_inorder((2 * index) + 1);
-        self.internal_inorder((2 * index) + 2);
+        None
     }
+}
 
-    pub fn search(&self, letter: char) -> String {
-        self.internal_search(0, letter)
-    }
+/// next() Iterator
+/// Used by iter() as it returns the index to the next key val pair
+impl<K, V> Iterator for GBST<K, V> {
+    type Item = usize;
+    fn next(&mut self) -> Option<usize> {
+        if self.current.is_some() || !self.stack.is_empty() || self.first {
+            self.first = false;
+            while self.current != None {
+                self.stack.push(self.current.unwrap());
+                let x = self.current.unwrap();
+                let y = x * 2 + 1;
+                if self.get_key(y).is_some() {
+                    self.current = Some(y);
+                } else {
+                    self.current = None;
+                }
+            }
+            self.current = self.stack.pop();
 
-    fn internal_search(&self, index: usize, letter: char) -> String {
-        let key = self.get_char(index);
-        let x = self.get_string(index);
-
-        let item = Pair::new(letter, "".to_string());
-
-        // we can't find it
-        // return empty string
-        if key == 0 as char {
-            return item.1;
+            let temp = self.current.unwrap();
+            let x = self.current.unwrap();
+            let y = x * 2 + 2;
+            if self.get_key(y).is_some() {
+                self.current = Some(y);
+            } else {
+                self.current = None;
+            }
+            return Some(temp);
         }
 
-        if item.0 == key {
-            return x;
-        } else if item.0 > key {
-            let mut new_index = 0;
-            if (2 * index) + 2 <= self.capacity {
-                new_index = (2 * index) + 2;
+        None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::GPair;
+    use super::GBST;
+
+    #[test]
+
+    fn is_empty() {
+        let mut bst: GBST<i32, i32> = GBST::new();
+        assert_eq!(true, bst.is_empty());
+        //bst.insert(Pair::new('A', "hello".to_string()));
+    }
+
+    #[test]
+    fn size() {
+        let mut bst: GBST<i32, i32> = GBST::new();
+        assert_eq!(0, bst.size());
+    }
+
+    #[test]
+    fn insert() {
+        let mut bst: GBST<i32, i32> = GBST::new();
+        assert_eq!(0, bst.size());
+        bst.insert(12, 12);
+        assert_eq!(1, bst.size());
+    }
+
+    #[test]
+    fn contains_key() {
+        let mut bst: GBST<&str, i32> = GBST::new();
+
+        bst.insert("Beetle", 0);
+        bst.insert("Cardinal", 0);
+        bst.insert("Giraffe", 0);
+        bst.insert("Kangaroo", 0);
+        bst.insert("Tiger", 0);
+        bst.insert("Aardvark", 0);
+        bst.insert("Dog", 0);
+        let x = bst.contains_key(&"Tiger");
+        assert_eq!(true, x.is_some());
+        let y = bst.get_key(x.unwrap()).unwrap();
+        assert_eq!("Tiger", *y);
+    }
+
+    #[test]
+    fn iter() {
+        let mut bst: GBST<&str, i32> = GBST::new();
+
+        bst.insert("Beetle", 0);
+        bst.insert("Cardinal", 0);
+        bst.insert("Giraffe", 0);
+        bst.insert("Kangaroo", 0);
+        bst.insert("Tiger", 0);
+        bst.insert("Aardvark", 0);
+        bst.insert("Dog", 0);
+
+        loop {
+            match bst.iter() {
+                Some(x) => {
+                    print!("{} ", x);
+                }
+                None => {
+                    println!("");
+                    break;
+                }
             }
-            return self.internal_search(new_index, letter);
-        } else {
-            let mut new_index = 0;
-            if (2 * index) + 1 <= self.capacity {
-                new_index = (2 * index) + 1;
-            }
-            return self.internal_search(new_index, letter);
         }
     }
 }
